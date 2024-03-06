abc6b2d5c9be63f1d96adcf6137bfdf6920ddf6f
DivyaShruthiM <muri0032@algonquinlive.com>
2024-03-05 22:24:27 -0500

    modified code to add watermark to grayscale image

--- WatermarkImage/watermarkimage.py
+++ WatermarkImage/watermarkimage.py
@@ -6,17 +6,16 @@ def add_watermark(input_image_path, output_image_path, watermark_text):
 
     font = ImageFont.truetype("arial.ttf", 50)
     text_width = draw.textlength(watermark_text, font)
-
     width, height = image.size
     x = (width - text_width) // 2
     y = (height - 50) // 2
 
     #add watermark to the image
-    draw.text((x, y), watermark_text, font=font, fill=(255, 255, 255, 128))
+    draw.text((x, y), watermark_text,font=font, stroke_fill='red',fill=255)
 
     image.save(output_image_path)
 
-input_image_path = "C:\\Users\\divya\\OneDrive\\Documents\\Cloud Development and Operations\\2nd sem\\Serverless Applications\\resized_image.jpg"
+input_image_path = "C:\\Users\\divya\\OneDrive\\Documents\\Cloud Development and Operations\\2nd sem\\Serverless Applications\\grayscale_image.jpg"
 output_image_path = "C:\\Users\\divya\\OneDrive\\Documents\\Cloud Development and Operations\\2nd sem\\Serverless Applications\\output_image_with_watermark.jpg"
 watermark_text = "Watermark"
 add_watermark(input_image_path, output_image_path, watermark_text)
